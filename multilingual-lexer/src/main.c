19125dc4297f47facb8d22320122308104a34875
24CSB0B37 <kr24csb0b37@student.nitw.ac.in>
2026-01-27 23:24:23 +0530

    Create main.c

---
+++ multilingual-lexer/src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "input_handler.h"
+#include "lang_config.h"
+#include "lexer.h"
+#include "token_output.h"
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        printf("Usage: mlexer <lang> <file>\n");
+        return 1;
+    }
+
+    FILE *src = open_source(argv[2]);
+    const KeywordEntry *keywords = load_language(argv[1]);
+
+    Token t;
+    while ((t = next_token(src, keywords)).type != TOK_UNKNOWN) {
+        emit_token(t);
+    }
+
+    fclose(src);
+    return 0;
+}
