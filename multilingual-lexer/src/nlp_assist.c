502daf180e111cadc735b3cf84a2da602540a7a6
24CSB0B37 <kr24csb0b37@student.nitw.ac.in>
2026-01-27 23:22:27 +0530

    Create nlp_assist.c

---
+++ multilingual-lexer/src/nlp_assist.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "nlp_assist.h"
+
+void analyze_ambiguity(const char *lexeme) {
+    /* Placeholder for future NLP-based analysis */
+    printf("[NLP Advisory] Ambiguous token: %s\n", lexeme);
+}
