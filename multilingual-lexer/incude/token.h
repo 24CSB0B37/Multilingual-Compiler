427c4a1e56a99b30322b3188be82e47c4a5b5c75
24CSB0B37 <kr24csb0b37@student.nitw.ac.in>
2026-01-27 23:25:22 +0530

    Add token.h for token type definitions and structures

---
+++ multilingual-lexer/incude/token.h
@@ -0,0 +1,25 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+typedef enum {
+    TOK_IF,
+    TOK_ELSE,
+    TOK_WHILE,
+    TOK_FOR,
+    TOK_RETURN,
+    TOK_INT,
+    TOK_FLOAT,
+    TOK_IDENTIFIER,
+    TOK_NUMBER,
+    TOK_SYMBOL,
+    TOK_UNKNOWN
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char lexeme[128];
+    int line;
+    int column;
+} Token;
+
+#endif
